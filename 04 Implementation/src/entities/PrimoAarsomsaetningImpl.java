962bff84e518c2c6f8148339634e0bc99f19a5da
Rasmus Jannerup <54896192+rasm211d@users.noreply.github.com>
2020-03-02 13:43:34 +0100

    Implementeret SD/DCD01013 i koden. Coauthor: Rasmus

---
+++ 04 Implementation/src/entities/PrimoAarsomsaetningImpl.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class PrimoAarsomsaetningImpl implements PrimoAarsomsaetning, Observable {
+    private double beloeb;
+    ObserverManager observerManager;
+
+    @Override
+    public void angivBeloeb(double beloeb) {
+        this.beloeb = beloeb;
+        observerManager.notificerObservere(this);
+    }
+
+    @Override
+    public void tilmeldObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void afmeldObserver(Observer observer) {
+
+    }
+
+
+}
