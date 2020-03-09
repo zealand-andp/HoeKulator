ba0197e291f082acff4ba17ebf866d179a50c310
Benjamin Kyhn <benj2605@edu.easj.dk>
2020-03-09 13:30:14 +0100

    Du kan nu tilføje og fjerne flere afskrivninger i UI

--- 04 Implementation/javafx/src/main/java/GrundUIController.java
+++ 04 Implementation/javafx/src/main/java/GrundUIController.java
@@ -7,10 +7,14 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class GrundUIController {
     private BeregnOmsaetningController beregnOmsaetningController;
     private BeregnAfskrivningController beregnAfskrivningController;
+    double nodeY = 38;
+    ArrayList<Node> afskrivninger;
+
 
     @FXML
     Label omsaetningResultatLabel1, omsaetningResultatLabel2;
@@ -19,6 +23,7 @@ public class GrundUIController {
     private Pane omsaetningPane, afskrivningPane;
 
     public void initialize() throws IOException {
+        afskrivninger = new ArrayList<>();
         loadOmsaetning();
         loadAfskrivning();
     }
@@ -35,11 +40,36 @@ public class GrundUIController {
         Node node = fxmlLoader.load();
         beregnAfskrivningController = fxmlLoader.getController();
         afskrivningPane.getChildren().add(node);
+        afskrivninger.add(node);
     }
 
     @FXML
     public void tilfoejTilResultatBudget(){
         omsaetningResultatLabel1.setText(String.valueOf(beregnOmsaetningController.getOmsaetning().hentOmsaetning()));
         omsaetningResultatLabel2.setText(omsaetningResultatLabel1.getText());
+    }
+
+    @FXML
+    public void tilfoejNyAfskrivning() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("beregnafskrivning/Tilfoej_dialog_base.fxml"));
+        Node node = fxmlLoader.load();
+        afskrivninger.add(node);
+        nodeY += 68;
+        beregnAfskrivningController = fxmlLoader.getController();
+        node.setLayoutY(nodeY);
+        afskrivningPane.setPrefHeight(afskrivningPane.getPrefHeight() + 68);
+        afskrivningPane.getChildren().add(node);
+
+    }
+
+    @FXML
+    public void fjernAfkrivning() throws IOException{
+        if (afskrivninger.size() <= 0){
+            return;
+        }
+        afskrivningPane.getChildren().remove(afskrivninger.get(afskrivninger.size() - 1));
+        afskrivninger.remove(afskrivninger.size()-1);
+        nodeY -= 68;
+        afskrivningPane.setPrefHeight(afskrivningPane.getPrefHeight() - 68);
     }
 }
