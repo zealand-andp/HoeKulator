59aed77a20262a3907ce5a6e4e8a850a4e6cc4a5
MarcKlesiewicz <marcklesiewicz21@gmail.com>
2020-03-09 14:59:11 +0100

    statet på implementering af UC02

--- 04 Implementation/src/entities/Vareforbrug.java
+++ 04 Implementation/src/entities/Vareforbrug.java
@@ -5,4 +5,6 @@ import entities.exceptions.NegativBeloebException;
 public interface Vareforbrug {
     void angivBeloeb(double beloeb) throws NegativBeloebException;
     double hentBeloeb();
+    void tilmeldObserver(Observer observer);
+    void afmeldObserver(Observer observer);
 }
