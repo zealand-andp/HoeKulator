624f20cb0f2e8056d017a0d5d23d758ee6a589b3
Benjamin Kyhn <benj2605@edu.easj.dk>
2020-03-11 14:39:32 +0100

    Fix kobling af UC01 til UI.
    Coauthors: Tommy, Benjamin
    Reviewer: Chad

--- 04 Implementation/src/beregnomsaetning/BeregnOmsaetningImpl.java
+++ 04 Implementation/src/beregnomsaetning/BeregnOmsaetningImpl.java
@@ -14,6 +14,16 @@ public class BeregnOmsaetningImpl implements BeregnOmsaetning {
     private PrimoAarsomsaetning primoAarsomsaetning;
     private Procentstigning procentstigning;
 
+    public BeregnOmsaetningImpl() {
+        vareforbrug = new VareforbrugImpl();
+        bruttofortjeneste = new BruttofortjenesteImpl();
+        afsaetning = new AfsaetningImpl();
+        salgspris = new SalgsprisImpl();
+        omsaetning = new OmsaetningImpl();
+        primoAarsomsaetning = new PrimoAarsomsaetningImpl();
+        procentstigning = new ProcentstigningImpl();
+    }
+
     @Override
     public void angivBruttofortjenesteOgVareforbrug(double bruttofortjeneste, double vareforbrug) throws NegativBeloebException {
         this.vareforbrug.angivBeloeb(vareforbrug);
@@ -33,8 +43,9 @@ public class BeregnOmsaetningImpl implements BeregnOmsaetning {
         this.primoAarsomsaetning.angivBeloeb(primoAarsomsaetning);
         this.procentstigning.angivDecimaltal(procentstigning);
         this.omsaetning.anvendPrimoAarsomsaetningOgProcentstigning(this.primoAarsomsaetning, this.procentstigning);
-
     }
 
-
+    public Omsaetning getOmsaetning() {
+        return omsaetning;
+    }
 }
