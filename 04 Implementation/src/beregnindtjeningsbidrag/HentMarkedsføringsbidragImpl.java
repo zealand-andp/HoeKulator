d8c45040289d67b10bf2b71247dec079aa381928
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2020-03-11 15:26:59 +0100

    3.Udgave af hentKKOImpl.java Coauthor: Nikolaj Reviewer: Ingen

--- 04 Implementation/src/beregnindtjeningsbidrag/HentMarkedsføringsbidragImpl.java
+++ 04 Implementation/src/beregnindtjeningsbidrag/HentMarkedsføringsbidragImpl.java
@@ -3,7 +3,7 @@ package beregnindtjeningsbidrag;
 public class HentMarkedsføringsbidragImpl {
 
     protected double hentMarkedsføringsbidrag(int markedsføringsbidrag){
-
+    return 1.0;
     } //TODO tilføj markedsføringsbidrags object til parametre og implement kode
 
 }
