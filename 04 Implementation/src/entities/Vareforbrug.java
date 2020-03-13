a7ba0d6d01a3fc6dcb472a97c81144e7b68fc3bb
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2020-03-13 11:33:43 +0100

    Rettede Vareforbrug og VareforbrugImpl

--- 04 Implementation/src/entities/Vareforbrug.java
+++ 04 Implementation/src/entities/Vareforbrug.java
@@ -4,7 +4,11 @@ import entities.exceptions.NegativBeloebException;
 
 public interface Vareforbrug {
     void angivBeloeb(double beloeb) throws NegativBeloebException;
+    void anvendIndkoebprisOgAfsaetning(Indkoebspris indkoebspris, Afsaetning afsaetning);
+    void anvendBruttofortjenesteOgOmsaetning(Bruttofortjeneste bruttofortjeneste, Omsaetning omsaetning);
+    void anvendVarelagerPrimoOgVarekoebOgVarelagerUltimo(VarelagerPrimo varelagerPrimo, Varekoeb varekoeb, VarelagerUltimo varelagerUltimo);
     double hentBeloeb();
     void tilmeldObserver(Observer observer);
     void afmeldObserver(Observer observer);
+    double hentVareforbrug();
 }
