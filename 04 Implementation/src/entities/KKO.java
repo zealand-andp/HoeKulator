856ce739aa271ff7103fbd6b0fdb1bbd1def6091
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-03-13 16:23:42 +0100

    Update 04 Implementation/src/entities/KKO.java
    Co-Authored-By: Anders Petersen <AndP@Zealand.dk>

--- 04 Implementation/src/entities/KKO.java
+++ 04 Implementation/src/entities/KKO.java
@@ -7,7 +7,7 @@ import entities.exceptions.NegativBeloebException;
 public interface KKO {
     boolean navnEksisterer(String navn);
     boolean foraelderEksisterer(String foraeldersNavn);
-    void tilføjkko(KKO nykko);
+    void tilfoejKKO(KKO nyKKO);
     void tilmeldObserver(Observer observer);
     void afmeldObserver(Observer observer);
     String hentNavn();
